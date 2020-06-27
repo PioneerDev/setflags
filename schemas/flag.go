92b3e0ae20254f0d8180d86fc7712c20a40e5d7c
linsanityHuang <js_huang2018@163.com>
2020-06-27 21:53:01 +0800

    update flag schema

--- schemas/flag.go
+++ schemas/flag.go
@@ -4,14 +4,14 @@ import "github.com/gofrs/uuid"
 
 // Flag validate flag json
 type Flag struct {
-	PayerID        uuid.UUID `json:"payer_id" binding:"required"`
-	PayerName      string    `json:"payer_name" binding:"required"`
-	PayerAvatarURL string    `json:"payer_avatar_url" binding:"required"`
+	PayerID        uuid.UUID `json:"payer_id"`
+	PayerName      string    `json:"payer_name"`
+	PayerAvatarURL string    `json:"payer_avatar_url"`
 	Task           string    `json:"task" binding:"required"`
 	Days           int       `json:"days" binding:"required"`
 	MaxWitness     int       `json:"max_witness" binding:"required"`
 	AssetID        uuid.UUID `json:"asset_id" binding:"required"`
 	Amount         float64   `json:"amount" binding:"required"`
 	TimesAchieved  int       `json:"times_achieved" binding:"required"`
-	Status         string    `json:"status" binding:"required"`
+	Status         string    `json:"status"`
 }
